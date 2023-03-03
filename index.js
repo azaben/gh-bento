9e5c7b71baa8351f601fbf489c9cfc5653e24f8d
azaben <126833658+azaben@users.noreply.github.com>
2023-03-04 00:09:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
-        var number = $('input[name="number"]:checked').val();
+        var time = $('input[name="time"]').val();
         var names = '';
-        $('#form-name').children().each(function (i, elm) {
+                        $('#form-name').children().each(function (i, elm) {
             names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
+        var number = $('input[name="number"]:checked').val();
+        
 
-        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
+        var msg = `受け取り日：${date}\n時間：${time}\n弁当名：${names}\n個数：${number}`;
         sendText(msg);
 
         return false;
